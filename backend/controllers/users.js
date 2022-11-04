0dadc4689ade60180d0406883e61a8d605f4029b
HamzaYousif <yous.hamze@gmail.com>
2022-11-04 21:34:32 +0300

    update_user

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -1,4 +1,5 @@
 const { pool } = require('../models/db')
+const bcrypt = require("bcrypt");
 
 const deleteUser = (req, res) => {
     const user_id = req.params.id;
@@ -26,4 +27,35 @@ const deleteUser = (req, res) => {
     })
 }
 
-module.exports = { deleteUser }
+const updateUser = async function(req, res){
+    const user_id = req.params.id;
+    let {first_name, last_name, email, password, gender} = req.body;
+    if(password){
+        password = await bcrypt.hash(password, 10);
+    }
+
+    const values = [user_id, first_name || null, last_name || null, email || null, password || null, gender || null];
+    const query = `UPDATE users SET first_name = COALESCE($2, first_name), last_name = COALESCE($3, last_name), email = COALESCE($4, email), password = COALESCE($5, password), gender = COALESCE($6, gender) WHERE id = $1 RETURNING *;`
+    
+    pool.query(query, values)
+    .then((result) => {
+        let successObject = {
+            success: true,
+            message: "User Updated Successfully",
+            result: result.rows
+          }
+      
+          res.status(201).json(successObject)
+    })
+    .catch((err) => {
+        let failObject = {
+            success: false,
+            message: "Server error",
+            err: err.message
+          }
+      
+          res.status(409).json(failObject)
+    })
+}
+
+module.exports = { deleteUser, updateUser }
