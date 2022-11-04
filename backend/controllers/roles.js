c5310de4f96a888f5707d3e871c6fb63237d1e35
HamzaYousif <yous.hamze@gmail.com>
2022-11-04 21:03:42 +0300

    add role_permission

--- backend/controllers/roles.js
+++ backend/controllers/roles.js
@@ -52,4 +52,30 @@ const addPermission = (req, res) => {
     })
 }
 
-module.exports = { addRole, addPermission }
+const addRolePermission = (req, res) => {
+    const {role_id, permission_id} = req.body;
+    const values = [role_id, permission_id];
+    const query = `INSERT INTO role_permission (role_id, permission_id) VALUES ($1, $2) RETURNING *`
+
+    pool.query(query, values)
+    .then((result) => {
+        let successObject = {
+            success: true,
+            massage: "role_permission Created Successfully",
+            result: result.rows
+          }
+      
+          res.status(201).json(successObject)
+    })
+    .catch((err) => {
+        let failObject = {
+            success: false,
+            massage: "Server error",
+            err: err.message
+          }
+      
+          res.status(409).json(failObject)
+    })
+}
+
+module.exports = { addRole, addPermission, addRolePermission }
