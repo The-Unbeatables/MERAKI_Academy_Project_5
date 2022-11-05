37c820a5946af4fb9c372474f91241e7799adf59
AshrafSAlalaqmeh <Ashraf.Alalaqmeh@gmail.com>
2022-11-05 17:25:38 +0200

    get and delete reviews

--- backend/routes/reviews.js
+++ backend/routes/reviews.js
@@ -1,9 +1,11 @@
 const express = require('express')
-const { addWorkerReview, updateReviewid } = require('../controllers/reviews')
+const { addWorkerReview, updateReviewid, deleteReviewById, getAllreviews } = require('../controllers/reviews')
 
 const reviewsRouter =express.Router()
 
 reviewsRouter.post('/', addWorkerReview)
 reviewsRouter.put('/:id' , updateReviewid)
+reviewsRouter.delete('/:id',deleteReviewById)
+reviewsRouter.get('/', getAllreviews)
 
 module.exports = reviewsRouter
