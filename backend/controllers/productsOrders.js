901316290b7e692d985467936069553d952afd1c
HamzaYousif <yous.hamze@gmail.com>
2022-11-06 18:37:56 +0300

    Update product order

--- backend/controllers/productsOrders.js
+++ backend/controllers/productsOrders.js
@@ -127,4 +127,33 @@ const getUserProductOrders = (req, res) => {
           res.status(409).json(failObject)
     })
 }
-module.exports = { addProductOrder, deleteProductOrder, deleteAllUserProductOrders, getAllProductOrders, getUserProductOrders }
+
+const updateProductOrder = (req, res) => {
+    const orderId = req.params.id;
+    const {user_id, quantity, product_id} = req.body;
+
+    const values = [orderId, user_id || null, quantity || null, product_id || null];
+    const query = `UPDATE product_orders SET user_id = COALESCE($2, user_id), quantity = COALESCE($3, quantity), product_id = COALESCE($4, product_id) WHERE id = $1 RETURNING *;`
+
+    pool.query(query, values)
+    .then((result) => {
+        let successObject = {
+            success: true,
+            massage: `order updated successfully`,
+            result: result.rows
+          }
+      
+          res.status(201).json(successObject)
+    })
+    .catch((err) => {
+        let failObject = {
+            success: false,
+            massage: "Server error",
+            err: err.message
+          }
+      
+          res.status(409).json(failObject)
+    })
+}
+
+module.exports = { addProductOrder, deleteProductOrder, deleteAllUserProductOrders, getAllProductOrders, getUserProductOrders, updateProductOrder }
