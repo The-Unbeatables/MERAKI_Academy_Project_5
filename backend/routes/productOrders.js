83b79f59567449b449ecc4ba3f14dcae165413f4
HamzaYousif <yous.hamze@gmail.com>
2022-11-06 18:31:08 +0300

    get all product orders for a certain user

--- backend/routes/productOrders.js
+++ backend/routes/productOrders.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { addProductOrder, deleteProductOrder, deleteAllUserProductOrders, getAllProductOrders } = require('../controllers/productsOrders');
+const { addProductOrder, deleteProductOrder, deleteAllUserProductOrders, getAllProductOrders, getUserProductOrders } = require('../controllers/productsOrders');
 
 const productOrderRouter = express.Router();
 
@@ -7,5 +7,6 @@ productOrderRouter.post('/', addProductOrder)
 productOrderRouter.delete('/:id', deleteProductOrder)
 productOrderRouter.delete('/all/:id', deleteAllUserProductOrders)
 productOrderRouter.get('/', getAllProductOrders)
+productOrderRouter.get('/:id', getUserProductOrders)
 
 module.exports = productOrderRouter;
