39ecb7e3b0cd3df7832fa739631e04270fad238d
AshrafSAlalaqmeh <Ashraf.Alalaqmeh@gmail.com>
2022-11-07 22:02:12 +0200

    ProductSlice

---
+++ frontend/src/redux/reducers/products/index.js
@@ -0,0 +1,35 @@
+import {creatSlice} from '@reduxjs/toolkit'
+
+const productSlice = creatSlice({
+    name: 'products',
+    initialState:{
+        products : [],
+    },
+    reducers:{
+        setProduct:(state , action)=>{
+            state.products = action.payload
+        },
+        addProduct:(state,action)=>{
+        state.products.push(action.payload);
+        },
+        updateProduct: (state , action)=> {
+            state.products = state.products.map((item)=>{
+            if(item.id === action.payload.id){
+                return action.payload   
+            }
+            else{
+                return item
+            }
+           })
+        },
+        deleteProduct:(state,action)=>{
+           state.products=state.products.filter((item)=>{
+            return item.id != action.payload
+           })
+        },
+         
+    }
+})
+
+export const {setProduct , addProduct , updateProduct , deleteProduct} =productSlice.actions
+export default productSlice.reducer
