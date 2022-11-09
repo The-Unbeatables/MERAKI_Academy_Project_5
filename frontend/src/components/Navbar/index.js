2629cbd2fb7606a6f248813e055c64e90d3d42d2
HamzaYousif <yous.hamze@gmail.com>
2022-11-09 18:35:40 +0300

    Navbar

--- frontend/src/components/Navbar/index.js
+++ frontend/src/components/Navbar/index.js
@@ -2,9 +2,12 @@ import React, {useState, useEffect} from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import "./style.css"
 import { useNavigate } from 'react-router-dom'
+import { setLogout } from '../../redux/reducers/auth';
 
 const Navbar = () => {
     const navigate = useNavigate();
+    const dispatch = useDispatch();
+
     const { isLoggedIn } = useSelector((state) => {
         return{
             isLoggedIn: state.auth.isLoggedIn
@@ -24,12 +27,12 @@ const Navbar = () => {
 
     {isLoggedIn && 
         <div className='nav-container'>
-        <div><h1>Home</h1></div>
+        <div onClick={() => {navigate('/')}}><h1>Home</h1></div>
         <div><h1>App Name</h1></div>
-        <div><h1>Wishlist</h1></div>
-        <div><h1>Cart</h1></div>
-        <div><h1>Services</h1></div>
-        <div><h1>Logout</h1></div>
+        <div onClick={() => {navigate('/cart')}}><h1>Wishlist</h1></div>
+        <div onClick={() => {navigate('/products/orders')}}><h1>Cart</h1></div>
+        <div onClick={() => {navigate('/services/orders')}}><h1>Services</h1></div>
+        <div onClick={() => {dispatch(setLogout())}}><h1>Logout</h1></div>
     </div>
     }
     </>
