4b31c782a4a289dbf7bf0e535bf1a4d40a7647b1
AshrafSAlalaqmeh <Ashraf.Alalaqmeh@gmail.com>
2022-11-09 18:14:32 +0200

    edit

--- frontend/src/components/ProductDetails/index.js
+++ frontend/src/components/ProductDetails/index.js
@@ -13,15 +13,19 @@ const {item}= useSelector((state)=>{
 
 
     return(
-        <div>
+        <div className="containerDetails">
         <h2 className="n">Product Details</h2>
         <div className="cardDetalis">
-        <div>{item.image}</div>
+        <div className="imgDetails"> 
+        <img src={`${item.image}`} />
+        </div>
+        <div >
         <div>{item.title}</div>
         <div>{item.price}</div>
         <div>{item.items_left}</div>
         <div>{item.category}</div>
         <div>{item.description}</div>
+        </div>
         </div>
         
         
