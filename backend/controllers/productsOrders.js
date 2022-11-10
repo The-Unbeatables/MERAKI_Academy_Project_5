7799057b43a37ede265caf639bd3796e595799ab
Odaitawalbeh <odai.mo.tawalbeh@gmail.com>
2022-11-10 23:27:12 +0300

    edit productOrder backEnd and route

--- backend/controllers/productsOrders.js
+++ backend/controllers/productsOrders.js
@@ -1,11 +1,12 @@
 const { pool } = require('../models/db')
 
 const addProductOrder = (req, res) => {
-    const {user_id, product_id} = req.body;
-    const values = [user_id, product_id];
-    const query = `INSERT INTO product_orders (user_id, product_id) VALUES ($1, $2) RETURNING *`
+    const user_id = req.token.userId;
+    const { product_id } = req.body;
+    const data = [product_id, user_id]
+    const query = `INSERT INTO product_orders (product_id, user_id) VALUES ($1, $2) RETURNING *`
 
-    pool.query(query, values)
+    pool.query(query, data)
     .then((result) => {
         let successObject = {
             success: true,
@@ -26,30 +27,25 @@ const addProductOrder = (req, res) => {
     })
 }
 const deleteProductOrder = (req, res) => {
-    const  orderId  = req.params.id;
-    const values = [orderId];
+    const cartId = req.params.id;
+    const data = [cartId]
     const query = `UPDATE product_orders SET is_deleted = 1 WHERE id = $1 RETURNING *;`
 
-    pool.query(query, values)
+    pool.query(query,data)
     .then((result) => {
-        let successObject = {
-            success: true,
-            massage: "Order deleted Successfully",
+        res.status(201).json({
+            succsess: true,
+            message: `Delete From The Cart`,
             result: result.rows
-          }
-      
-          res.status(201).json(successObject)
+        })
     })
     .catch((err) => {
-        let failObject = {
-            success: false,
-            massage: "Server error",
-            err: err.message
-          }
-      
-          res.status(409).json(failObject)
+        res.status(500).json({
+            succsess: false,
+            error: err.message
+        })
     })
-}
+};
 
 const deleteAllUserProductOrders = (req, res) => {
     const  userId  = req.params.id;
@@ -79,7 +75,7 @@ const deleteAllUserProductOrders = (req, res) => {
 
 const getAllProductOrders = (req, res) => {
 
-    const query = `SELECT * FROM product_orders INNER JOIN products ON product_orders.user_id= products.id;`
+    const query = `SELECT * FROM product_orders INNER JOIN users ON product_orders.user_id= users.id;`
 
     pool.query(query)
     .then((result) => {
@@ -103,15 +99,14 @@ const getAllProductOrders = (req, res) => {
 }
 
 const getUserProductOrders = (req, res) => {
-    const userId = req.params.id;
-    const values = [userId]
-    const query = `SELECT * FROM product_orders INNER JOIN products ON product_orders.user_id= products.id WHERE user_id = $1;`
+    const user_id = req.token.userId
+    const data = [user_id]
+    const query = `SELECT * FROM products INNER JOIN product_orders ON products.id = product_orders.product_id WHERE product_orders.is_deleted =0 AND user_id = $1;`
 
-    pool.query(query, values)
+    pool.query(query, data)
     .then((result) => {
         let successObject = {
             success: true,
-            massage: `All orders for ${result.rows[0].first_name}`,
             result: result.rows
           }
       
