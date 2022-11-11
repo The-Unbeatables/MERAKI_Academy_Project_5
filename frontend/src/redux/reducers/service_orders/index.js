eccc4b86e59571cb46c4ac248894c9bf6bb4410d
HamzaYousif <yous.hamze@gmail.com>
2022-11-11 22:50:07 +0300

    Services component

--- frontend/src/redux/reducers/service_orders/index.js
+++ frontend/src/redux/reducers/service_orders/index.js
@@ -4,7 +4,8 @@ const serviceOrdersSlice = createSlice({
     name: "serviceOrders",
     initialState: {
         workerServiceOrders: [],
-        allServiceOrders: []
+        allServiceOrders: [],
+        userServiceOrders: []
     },
     reducers: {
         setWorkerServiceOrders: (state, action) => {
@@ -32,9 +33,30 @@ const serviceOrdersSlice = createSlice({
         },
         getAllServiceOrders: (state, action) => {
             state.allServiceOrders = action.payload
+        },
+
+        setUserServiceOrders: (state, action) => {
+            state.userServiceOrders = action.payload
+        },
+        deleteUserServiceOrder: (state, action) => {
+            state.userServiceOrders = state.userServiceOrders.filter((elem, i) => {
+                return elem.id !== action.payload
+            })
+        },
+
+        updateUserServiceOrder: (state, action) => {
+            state.userServiceOrders = state.userServiceOrders.map((elem, i) => {
+                if(elem.id === action.payload.id){
+                    return action.payload
+                }else{
+                    return elem
+                }
+            })
         }
     }
 })
 
 export default serviceOrdersSlice.reducer;
-export const { setWorkerServiceOrders, addWorkerServiceOrder, deleteWorkerServiceOrder, updateWorkerServiceOrder, getAllServiceOrders } = serviceOrdersSlice.actions;
+export const { setWorkerServiceOrders, addWorkerServiceOrder, deleteWorkerServiceOrder,
+     updateWorkerServiceOrder, getAllServiceOrders,
+     setUserServiceOrders, deleteUserServiceOrder, updateUserServiceOrder } = serviceOrdersSlice.actions;
