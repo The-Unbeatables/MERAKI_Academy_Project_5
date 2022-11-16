7b3c6061951bd3b80feb4b90aaaa1a6882fe3a5e
Hamza Yousif <yous.hamze@gmail.com>
2022-11-16 21:53:58 +0300

    ADdin

--- backend/routes/productOrders.js
+++ backend/routes/productOrders.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { addProductOrder, deleteProductOrder, deleteAllUserProductOrders, getAllProductOrders, getUserProductOrders, updateProductOrder } = require('../controllers/productsOrders');
+const { addProductOrder, deleteProductOrder, deleteAllUserProductOrders, getAllProductOrders, getUserProductOrders, updateProductOrder, getAllProductOrdersForAdmin } = require('../controllers/productsOrders');
 const authentication = require("../middlewares/authentication");
 const authorization = require("../middlewares/authorization")
 const productOrderRouter = express.Router();
@@ -10,6 +10,8 @@ productOrderRouter.delete('/all/order',  authentication, authorization("DELETE_P
 productOrderRouter.get('/', getAllProductOrders)
 productOrderRouter.get('/showCart', authentication, getUserProductOrders)
 productOrderRouter.put('/:id', authentication, authorization("UPDATE_PRODUCT_ORDER"), updateProductOrder)
+productOrderRouter.get('/all/admin', getAllProductOrdersForAdmin)
+
 
 module.exports = productOrderRouter;
 
