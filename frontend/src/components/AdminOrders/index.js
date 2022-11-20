aace98541ddb3072cac869437e410432f84d0d7b
Hamza Yousif <yous.hamze@gmail.com>
2022-11-20 23:11:39 +0300

    fix Admin issue

--- frontend/src/components/AdminOrders/index.js
+++ frontend/src/components/AdminOrders/index.js
@@ -1,34 +1,27 @@
 import axios from 'axios'
 import React, { useEffect } from 'react'
+import { useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { getAllServiceOrders } from '../../redux/reducers/service_orders'
 import './style.css'
 
 const AdminOrders = () => {
   const dispatch = useDispatch()
+  const [workers, setWorkers] = useState([])
 
   const {allServiceOrders }= useSelector((state)=>{
     return{
       allServiceOrders: state.serviceOrders.allServiceOrders,
-   
-
     }
   })
-
-
-
-  
-
-
-
-
-
 
 const getServiceOrders=()=>{
   axios.get('http://localhost:5000/ServiceOrders')
   .then((result)=>{
-   console.log(result.data.result);
-     dispatch(getAllServiceOrders(result.data.result))
+   console.log(result.data.result[0].rows);
+   console.log(result.data.result[1].rows);
+   setWorkers(result.data.result[1].rows)
+     dispatch(getAllServiceOrders(result.data.result[0].rows))
   
   })
   .catch((err)=>{
@@ -57,15 +50,24 @@ useEffect(()=>{
           </tr>
         </thead>
     <tbody>
-          { allServiceOrders?.map((item) => (
+          { allServiceOrders?.map((item,i) => (
               
-              <tr key={item.id}>
+              <tr key={i}>
             
-
                 <td data-label="Title">{item.service_title}</td>
                 <td data-label="Title">{item.service_description}</td>
                 <td className='price' data-label="Price"><b>{item.created_at.split('T')[0]}</b></td>
-                <td className='price' data-label="Price"><b>{item.worker_id}</b></td>
+
+                {workers.map((elem, index) => {
+                  if(item.worker_id === elem.id){
+                    return(
+                      <td className='price' data-label="Price"><b>{elem.first_name}</b></td>
+                    )
+                  }
+                })} 
+                  
+                 
+
                 <td className='price' data-label="Price"><b>{item.first_name}</b></td>
                 <td className='price' data-label="Price"><b>{item.status}</b></td>
                 <td data-label="Actions">
